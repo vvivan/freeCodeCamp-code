8f89839a252beac7a02de5f9fa0bf1e178275d93
Ivan Videnovic <videnovici@yahoo.com>
2017-12-06 15:55:07 +0100

    Bug Fixing

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function titleCase(str) {
+    var array = [];
+
+    array = str.split(' ');
+
+    for (let i = 0; i < array.length; i++) {
+        array[i] = sentenceCase(array[i]);
+    }
+    
+    str = array.join(' ');
+    console.log(str);
+    
     return str;
+  }
+
+  function sentenceCase(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
   
   titleCase("I'm a little tea pot");
